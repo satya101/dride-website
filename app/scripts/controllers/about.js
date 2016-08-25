46b3bc2a1bac3fa073f879bc443374c2f0d7c758
Yossi <saoron@gmail.com>
2016-08-25 17:31:48 +0300

    Reinitialise website [Awesomeness ahead] :rocket:

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name drideApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the drideApp
+ */
+angular.module('drideApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
