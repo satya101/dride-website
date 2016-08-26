e4eb9d3dfe4c1db352e6d498c34cd0d6bbebd96b
Yossi <saoron@gmail.com>
2016-08-26 10:38:09 +0300

    Added views transition

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,39 @@
  * Controller of the drideApp
  */
 angular.module('drideApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+
+
+        $scope.toLeft = false;
+        $scope.toRight = false;
+  
+        $scope.displayCard = 1;
+        
+        //when press prev, card slide to left
+        $scope.prev = function() {
+          $scope.toLeft = true;
+          $scope.toRight = false;
+          if ($scope.displayCard == 1) {
+            $scope.displayCard = 3
+          } else {
+            $scope.displayCard -= 1;
+          }
+        };
+        
+        //when press prev, card slide to right
+        $scope.next = function() {
+          $scope.toLeft = false;
+          $scope.toRight = true;
+  
+          if ($scope.displayCard == 3) {
+            $scope.displayCard = 1
+          } else {
+            $scope.displayCard += 1;
+          }
+        };
+
+
+
+
+
   });
