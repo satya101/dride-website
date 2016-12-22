7081f5aa3246948fffa358a868ae032003bf703d
Yossi <saoron@gmail.com>
2016-12-22 10:17:15 +0200

    Add edit button to docs

--- app/scripts/controllers/documentation.js
+++ app/scripts/controllers/documentation.js
@@ -21,4 +21,9 @@ angular.module('drideApp')
 		$scope.show = true;
 	}, 100);  
 
-  });
+  }).directive('editPage',function($rootScope){
+     return function(scope, element, attrs){
+          element.prepend('<div class="editDocsLabel"><a href="https://github.com/dride/dride-website/blob/master/app/views/content/'+($rootScope.currentPage.split('/'))[2]+'.html">Edit</a></div>')
+
+      }
+})
