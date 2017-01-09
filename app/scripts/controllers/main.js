c487b875def7a9906708d5c65643d202bec7e2ca
Yossi <saoron@gmail.com>
2017-01-09 09:57:35 +0200

    close #12

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -31,8 +31,11 @@ angular.module('drideApp')
 
         $scope.views = ["product", "cloud", "mic", "camera", "wifi", "app", "docs"]
         //when press prev, card slide to right
-        $scope.goToView = function(view, strict) {
+        $scope.goToView = function(view, strict, addOrSub) {
 
+            view = addOrSub ? (parseInt(view) + addOrSub) : view;
+            
+            console.log(view)
             //dont run if popup is there
             if ($scope.showPreOrder)
                 return;
@@ -54,6 +57,7 @@ angular.module('drideApp')
             $scope.toRight = false;
 
             $scope.displayCard = view;
+            console.log($scope.displayCard)
 
             //save the time of the transition to prevent super fast scroll
             $scope.transitionFired = dateNow
