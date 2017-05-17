ffcbd7dd5370d7f90ea904eb9451ac932bf98a6f
Yossi <saoron@gmail.com>
2017-05-17 15:11:33 +0300

    promise on question ask

--- app/scripts/services/askquestion.js
+++ app/scripts/services/askquestion.js
@@ -31,34 +31,41 @@ angular.module('drideApp')
 
 
                     $scope.closeModal = function() {
-
-                        $uibModalInstance.dismiss('cancel');
+                        $uibModalInstance.close();
                     };
 
+
+                    $scope.dismissModal = function() {
+                        $uibModalInstance.dismiss();
+                    };
 
                     //TODO: Add animation
                     $scope.openThread = function(title) {
 
-                    	login.verifyLoggedIn();
+                          login.verifyLoggedIn().then(
+                               function(result) {
 
-                        //add a new thread on Firebase
-                        var ref = firebase.database().ref("threads");
-                        var list = $firebaseArray(ref);
+                                    //add a new thread on Firebase
+                                    var ref = firebase.database().ref("threads");
+                                    var list = $firebaseArray(ref);
 
-                        list.$add({ 'title': title, 'created': new Date().getTime(), 'views': 0, 'participants': [$rootScope.firebaseUser.uid], 'description': '', 'cmntsCount': 1, 'lastUpdate': (new Date).getTime() }).then(function(ref) {
-                            list[list.$indexFor(ref.key)].slug = $scope.slugify(title, ref.key);
-                            list.$save(list.$indexFor(ref.key)).then(function(ref) {
+                                    list.$add({ 'title': title, 'created': new Date().getTime(), 'views': 0, 'participants': [$rootScope.firebaseUser.uid], 'description': '', 'cmntsCount': 1, 'lastUpdate': (new Date).getTime() }).then(function(ref) {
+                                        list[list.$indexFor(ref.key)].slug = $scope.slugify(title, ref.key);
+                                        list.$save(list.$indexFor(ref.key)).then(function(ref) {
+                                            $scope.closeModal();
+                                            //redirect to the thread slug
+                                            $location.path('thread/' + $scope.slugify(title, ref.key));
 
-                                $scope.closeModal();
-                                //redirect to the thread slug
-                                $location.path('thread/' + $scope.slugify(title, ref.key));
+                                            $mixpanel.track('posted a new post');
 
-                                $mixpanel.track('posted a new post');
-
-                            });
+                                        });
 
 
-                        });
+                                    });
+
+                                });
+
+
 
                     };
 
