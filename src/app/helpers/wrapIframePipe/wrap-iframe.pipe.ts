a4df20e1eb989cbd88f17a37a9c65ba4c5d473db
Yossi <saoron@gmail.com>
2017-12-25 10:04:45 +0200

    fixes

--- src/app/helpers/wrapIframePipe/wrap-iframe.pipe.ts
+++ src/app/helpers/wrapIframePipe/wrap-iframe.pipe.ts
@@ -7,9 +7,12 @@ export class WrapIframePipe implements PipeTransform {
 
 	transform(value: any, args?: any): any {
 
-		const b = value.match(/(<iframe.+?<\/iframe>)/g)[0]
-		console.log(b)
-		return value.replace(b, '<div class="wrapIframe">' + b + '</div>');
+		const b = value.match(/(<iframe.+?<\/iframe>)/g)
+		if (b) {
+			return value.replace(b, '<div class="wrapIframe">' + b + '</div>');
+		}else {
+			return value
+		}
 	}
 
 }
